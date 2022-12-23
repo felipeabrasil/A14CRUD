c248409ac7712d9c9bd62ffcd6d53ebd22d00618
felipeabrasil <felbrasil@gmail.com>
2022-12-23 11:47:06 -0300

    config routing base e routing crud module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:"", component: HomeComponent}
+  { path: '', redirectTo: 'crud', pathMatch: 'full' },
+  { path: 'crud', loadChildren: () => import('./crud/crud.module').then(m => m.CRUDModule) }
 ];
 
 @NgModule({
