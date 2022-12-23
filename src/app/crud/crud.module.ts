6cc4994e8b125321ef0def036e0d05815d73eaba
felipeabrasil <felbrasil@gmail.com>
2022-12-23 11:03:14 -0300

    create components, form, list, details

---
+++ src/app/crud/crud.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CRUDRoutingModule } from './crud-routing.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+
+
+@NgModule({
+  declarations: [
+
+
+    ProductDetailsComponent,
+    ProductFormComponent,
+    ProductListComponent
+  ],
+  imports: [
+    CommonModule,
+    CRUDRoutingModule
+  ]
+})
+export class CRUDModule { }
